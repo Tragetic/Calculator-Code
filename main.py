68d3823ca9f2b039a778f2dde30ef7c7340bd317
Jacob Bailey <>
2021-06-08 06:25:48 +0000

    Documentation

--- main.py
+++ main.py
@@ -75,5 +75,10 @@ Looking at the code now, especially the while true loop, it looks really simple,
 So what the program does is you can select a number, 1 through 9, and then you can either press the + button to add it by itself, or press the * button to multiply it by itself. Then when you click the X it closes the window. I didn't make the - or / buttons functional, because the result for all of them would be 0 so there wouldn't really be a point.
 
 
-I think to make it a real calculator, I would have to make it so that as long as you click integers, it stores them as a list. Then when you click an operator it starts a new list, then when you click enter it would have to convert both of those lists and then output the result.
+I think to make it a real calculator, I would have to make it so that as long as you click integers, it stores them as a list. Then when you click an operator it starts a new list, then when you click enter it would have to convert both of those lists into integers, then do the operation, and then output the result.
+
+But, it's 11:10pm on the 7th, tomorrow is my last day of high school ever, and I just now got a functional MVP. Honestly I'm just going to turn it in and call it good. 
+It's not that I just did the visual parts at the beggining of may, then didn't do anything, then did some crunch time at the last moment, even though that's definantly what it looks like. I put in time regularly throughout may and the first week of june, its just that I tried lots of different things and none of them worked, because I didn't know how to do what I wanted to do.
+Honestly I should've asked for help in like mid may, but I didn't. 
+Because I would say to myself "ok jacob it's time to do some coding", then I would spend 2 hours googling how to get the buttons to do what I want them to, and then I would give up, and then 2 or 3 days later I would try again and I would really not want to bother. I thought my problem was procrastintion but looking back it was just that I didn't know where to go. It's completly my fault for not having a functioning calculator, because I should've asked for help.
 """
